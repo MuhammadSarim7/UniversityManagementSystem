7e32bedbaa0dad03e73737ce0d8c303ddf9885a4
MuhammadSarim07 <f223688@cfd.nu.edu.pk>
2024-10-19 09:56:08 +0000

    Student Class Created

--- UniversityManagementSystem.py
+++ UniversityManagementSystem.py
@@ -2,3 +2,11 @@ class Human:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
+class Student(Human):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+
